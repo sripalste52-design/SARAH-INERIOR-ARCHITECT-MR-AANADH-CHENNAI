51ae66a8e4c25ffaed772dcc63bec6093fbce7ad
sripalste52-design <sripalste.52@gmail.com>
2026-09-08 22:26:09 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,465 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living",
+      "name": "LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6173443323355574,
+          "pitch": -0.02881537408274859,
+          "rotation": 4.71238898038469,
+          "target": "4-kitchen-1"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 3.141592653589793,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": 0.32397457989711853,
+          "pitch": 1.2760956735746731e-8,
+          "rotation": 4.71238898038469,
+          "target": "2-dining"
+        },
+        {
+          "yaw": -1.0744735928920797,
+          "pitch": -0.0648451460589925,
+          "rotation": 0,
+          "target": "7-stair-case"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-tv-unit",
+      "name": "LIVING TV UNIT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2589841808503834,
+          "pitch": 0.020623717453711166,
+          "rotation": 0,
+          "target": "7-stair-case"
+        },
+        {
+          "yaw": 1.899470968958683,
+          "pitch": 0.01710350277154049,
+          "rotation": 0,
+          "target": "5-kitchen-2"
+        },
+        {
+          "yaw": 3.0899277914836265,
+          "pitch": -0.02496288612159603,
+          "rotation": 1.5707963267948966,
+          "target": "6-pooja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dining",
+      "name": "DINING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5494249481990439,
+          "pitch": 0.010012082820189505,
+          "rotation": 3.141592653589793,
+          "target": "5-kitchen-2"
+        },
+        {
+          "yaw": 0.712770434171528,
+          "pitch": 0.03828223182747337,
+          "rotation": 0,
+          "target": "7-stair-case"
+        },
+        {
+          "yaw": -0.1745321532538444,
+          "pitch": 0.09492544493287625,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-2",
+      "name": "LIVING 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3292595743124629,
+          "pitch": 0.16299915415291544,
+          "rotation": 0,
+          "target": "4-kitchen-1"
+        },
+        {
+          "yaw": -0.33977250310183393,
+          "pitch": 0.15970916870283602,
+          "rotation": 0,
+          "target": "7-stair-case"
+        },
+        {
+          "yaw": -1.1742346247865925,
+          "pitch": 0.41366988192829623,
+          "rotation": 0,
+          "target": "0-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen-1",
+      "name": "KITCHEN 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.041215955582963204,
+          "pitch": 0.0029431542138809164,
+          "rotation": 0,
+          "target": "5-kitchen-2"
+        },
+        {
+          "yaw": 3.0425490182489483,
+          "pitch": -0.022527186345426387,
+          "rotation": 0,
+          "target": "6-pooja"
+        },
+        {
+          "yaw": -2.3872125501649784,
+          "pitch": 0.017235548197547246,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kitchen-2",
+      "name": "KITCHEN 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0793655417599588,
+          "pitch": 0.13137631558081608,
+          "rotation": 3.141592653589793,
+          "target": "4-kitchen-1"
+        },
+        {
+          "yaw": -0.3449990363674118,
+          "pitch": 0.011087917278118198,
+          "rotation": 0,
+          "target": "6-pooja"
+        },
+        {
+          "yaw": 0.4547933568031848,
+          "pitch": 0.03703046387957265,
+          "rotation": 1.5707963267948966,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pooja",
+      "name": "POOJA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.836841730430832,
+          "pitch": -0.007855749992163652,
+          "rotation": 7.853981633974483,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": 2.3751690607460336,
+          "pitch": 0.04222218607114847,
+          "rotation": 0,
+          "target": "7-stair-case"
+        },
+        {
+          "yaw": 3.094202468170373,
+          "pitch": 0.06520989016249068,
+          "rotation": 0,
+          "target": "4-kitchen-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-stair-case",
+      "name": "STAIR CASE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14623206349828521,
+          "pitch": 0.3697638119033613,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": 0.1777885256518985,
+          "pitch": -0.21133937769035782,
+          "rotation": 0,
+          "target": "8-first-floor"
+        },
+        {
+          "yaw": 0.18633575888090803,
+          "pitch": 0.014472344387243652,
+          "rotation": 1.5707963267948966,
+          "target": "9-rooms"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-first-floor",
+      "name": "FIRST FLOOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 1.5707963267948966,
+          "target": "9-rooms"
+        },
+        {
+          "yaw": -3.138192661540465,
+          "pitch": 0.030036248460568515,
+          "rotation": 3.141592653589793,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-rooms",
+      "name": "ROOMS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 3.141592653589793,
+          "target": "8-first-floor"
+        },
+        {
+          "yaw": 1.339744224350225,
+          "pitch": 0.7791607413949624,
+          "rotation": 3.141592653589793,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mr Aanadh Chennai",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
